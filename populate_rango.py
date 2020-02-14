b355e07b3450f6f38b4f47fbcd6311bf5251685c
Calum Corrie <2393272c@student.gla.ac.uk>
2020-02-14 13:03:11 +0000

    TWD 6.END, Tests:OK

--- populate_rango.py
+++ populate_rango.py
@@ -14,20 +14,20 @@ def populate():
 	# through each data structure, and add the data to our models.
 	
 	python_pages= [
-		{'title':'Official Python Tutorial','url':'http://docs.python.org/3/tutorial/'},
-		{'title':'How to Think like a Computer Scientist','url':'http://www.greenteapress.com/thinkpython/'},
-		{'title':'Learn Python in 10 Minutes','url':'http://www.korokithakis.net/tutorials/python/'}
+		{'title':'Official Python Tutorial','url':'http://docs.python.org/3/tutorial/', "views":42 },
+		{'title':'How to Think like a Computer Scientist','url':'http://www.greenteapress.com/thinkpython/', "views":43 },
+		{'title':'Learn Python in 10 Minutes','url':'http://www.korokithakis.net/tutorials/python/', "views":44 }
 	]
 		
 	django_pages = [
-		{'title':'Official Django Tutorial','url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-		{'title':'Django Rocks','url':'http://www.djangorocks.com/'},
-		{'title':'How to Tango with Django','url':'http://www.tangowithdjango.com/'}
+		{'title':'Official Django Tutorial','url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', "views":45 },
+		{'title':'Django Rocks','url':'http://www.djangorocks.com/', "views":46 },
+		{'title':'How to Tango with Django','url':'http://www.tangowithdjango.com/', "views":47 }
 	]
 
 	other_pages= [
-		{'title':'Bottle','url':'http://bottlepy.org/docs/dev/'},
-		{'title':'Flask','url':'http://flask.pocoo.org'}
+		{'title':'Bottle','url':'http://bottlepy.org/docs/dev/', "views":48 },
+		{'title':'Flask','url':'http://flask.pocoo.org', "views":49 }
 	]
 	
 	cats = {'Python': {'pages': python_pages, "views":128, "likes":64 },
@@ -38,7 +38,7 @@ def populate():
 	for cat,cat_data in cats.items():
 		c = add_cat(cat,cat_data["views"],cat_data["likes"])
 		for p in cat_data['pages']:
-			add_page( c, p['title'], p['url'] )
+			add_page( c, p['title'], p['url'], p['views'] )
 	
 	# Print out the categories we have added.
 	for c in Category.objects.all():
